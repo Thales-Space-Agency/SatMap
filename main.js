7659554266625a9f8145208d0c691d2e1f71a7ef
Teyk0o <theovilainfr@gmail.com>
2024-01-15 12:03:18 +0100

    fix: env variables

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ function animate() {
 }
 
 function getAllSatellites() {
-    fetch('http://192.168.1.148:3001/api/get-sat')
+    fetch(`${process.env.API_URL}/api/get-sat`)
         .then(response => response.json())
         .then(data => {
             satellites = data;
@@ -81,7 +81,7 @@ function getAllSatellites() {
 
 
 function updateSatellites() {
-    fetch('http://192.168.1.148:3001/api/update-sat', { method: 'POST' })
+    fetch(`${process.env.API_URL}/api/update-sat`, { method: 'POST' })
         .then(response => response.text())
         .then(data => console.log(data))
         .catch(error => console.error(error));
